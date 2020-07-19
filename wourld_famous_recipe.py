f53ff33c62f49ef30f92d32a06b4385806c78f49
salaandora <50085010+salaandora@users.noreply.github.com>
2020-07-18 21:28:18 -0400

    Create wourld_famous_recipe.py

---
+++ wourld_famous_recipe.py
@@ -0,0 +1,8 @@
+###prints my famous recipe for you to make at home###
+
+ingredients = ["hot chip", "green onion", "sea salt", "avocado"]
+print("My famous recipe:")
+for i in ingredients:
+    temp = 1
+    ing = str(temp) + ". " + i
+    print(ing)
